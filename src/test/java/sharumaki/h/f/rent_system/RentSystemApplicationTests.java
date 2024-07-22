fa69141ffcab33e71039d631b15ada71fd49ea7b
Federico Herrera <fe_de_30@hotmail.com>
2024-07-22 19:58:17 -0300

    First commit

---
+++ src/test/java/sharumaki/h/f/rent_system/RentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sharumaki.h.f.rent_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
